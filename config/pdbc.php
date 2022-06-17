51d7ceb1e90ea46eee531b2a04a919ec66f0b9c6
BlgnrSoftDev <19360859036@ogrenci.btu.edu.tr>
2022-06-17 14:52:36 +0300

    update

--- config/pdbc.php
+++ config/pdbc.php
@@ -1,8 +1,8 @@
 <?php
 
 define('DB_HOST', 'sql203.epizy.com');
-define('DB_USER', 'epiz_31976331');
-define('DB_PASS', 'hFLWGTaTtT');
+define('DB_USER', '');
+define('DB_PASS', '');
 define('DB_NAME', 'epiz_31976331_SecDon');
 
 $conn =  mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
